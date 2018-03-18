3d0876ad2db405732f0de00dc6ccc09135f0fb9f
JaylenHsieh <zjjxxiejunliang@gmail.com>
2018-03-18 16:36:39 +0800

    一些小修改

---
+++ app/src/main/java/com/hdu/newe/here/page/main/leavenoto/LeaveNotoActivity.kt
@@ -0,0 +1,13 @@
+package com.hdu.newe.here.page.main.leavenoto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hdu.newe.here.R
+
+class LeaveNotoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_leave_noto)
+    }
+}
---
+++ app/src/main/java/com/hdu/newe/here/page/main/leavenoto/LeaveNotoPresenter.java
@@ -0,0 +1,8 @@
+package com.hdu.newe.here.page.main.leavenoto;
+
+/**
+ * Created by Jaylen Hsieh on 2018/03/16.
+ */
+
+public class LeaveNotoPresenter {
+}
