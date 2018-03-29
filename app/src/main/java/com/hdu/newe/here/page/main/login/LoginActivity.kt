55448214db6f278e826056e195e476de0913fe12
JaylenHsieh <zjjxxiejunliang@gmail.com>
2018-03-29 22:53:37 +0800

    设计了登录界面
    更改申报书上的登录逻辑，取消了手机号步骤，直接用学号和IMEI绑定，个人中心界面更改手机号变为更改手机

---
+++ app/src/main/java/com/hdu/newe/here/page/main/login/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.hdu.newe.here.page.main.login
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.hdu.newe.here.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
