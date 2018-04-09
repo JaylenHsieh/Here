b7be08c5fa4c18b0b808d10bb28c4a9af5c89a4f
pope <1173038073@qq.com>
2018-04-09 23:11:17 +0800

    更改了更换手机的Dialog的文件名，加入了动态获取手机电话权限

--- app/src/main/java/com/hdu/newe/here/page/main/login/LoginFragment.kt
+++ app/src/main/java/com/hdu/newe/here/page/main/login/LoginFragment.kt
@@ -1,10 +1,16 @@
 package com.hdu.newe.here.page.main.login
 
 
+import android.content.Context
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.provider.Telephony
+import android.support.v4.content.ContextCompat.checkSelfPermission
+import android.telephony.TelephonyManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 
 import com.hdu.newe.here.R
 import com.hdu.newe.here.biz.user.entity.UserBean
@@ -18,10 +24,25 @@ class LoginFragment : BaseFragment<LoginContract.Presenter>(), LoginContract.Vie
 
     private var user: UserBean by Delegates.notNull()
 
+    private val PERMISSION_REQUEST_CALL_PHONE = 1
+
+    private val telManager:TelephonyManager = activity?.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val view =  inflater.inflate(R.layout.fragment_login, container, false)
         return view
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (checkSelfPermission(context!!, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(
+                    arrayOf(android.Manifest.permission.CALL_PHONE),
+                    PERMISSION_REQUEST_CALL_PHONE)
+        } else {
+            getImei()
+        }
     }
 
     override fun render() {
@@ -36,4 +57,21 @@ class LoginFragment : BaseFragment<LoginContract.Presenter>(), LoginContract.Vie
 //            user.isTeacher = view?.findViewById<CheckBox>(R.id.)?.isChecked ?: true
 //        }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == PERMISSION_REQUEST_CALL_PHONE){
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                getImei()
+            }else{
+                Toast.makeText(context,"    权限被拒绝", Toast.LENGTH_SHORT).show()
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun getImei(){
+//        telManager.getImei()
+    }
+
 }
--- app/src/main/java/com/hdu/newe/here/page/main/profile/EditPhoneNumberDialog.kt
+++ app/src/main/java/com/hdu/newe/here/page/main/profile/EditPhoneNumberDialog.kt
@@ -3,9 +3,7 @@ package com.hdu.newe.here.page.main.profile
 import android.app.AlertDialog
 import android.os.Bundle
 import android.support.v4.app.DialogFragment
-import android.view.ViewGroup
 import com.hdu.newe.here.R
-import com.hdu.newe.here.utils.UIUtils
 
 /**
  * Created by Jaylen Hsieh on 2018/03/26.
@@ -18,7 +16,7 @@ class EditPhoneNumberDialog: DialogFragment(){
 
     override fun onCreateDialog(savedInstanceState: Bundle?):AlertDialog {
         val builder = AlertDialog.Builder(activity)
-        val inflate = activity?.layoutInflater?.inflate(R.layout.fragment_edit_phone_number_dialog, null, false)
+        val inflate = activity?.layoutInflater?.inflate(R.layout.fragment_change_phone_dialog, null, false)
         return builder.setView(inflate)
                 .setCancelable(true)
                 .setMessage("申请更换手机")
